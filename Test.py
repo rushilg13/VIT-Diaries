b84a1d0c66d25c02106bb4736197a8b144f0dad3
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-24 22:20:33 +0530

    PyToMongo

---
+++ Test.py
@@ -0,0 +1,17 @@
+import pymongo
+cluster = pymongo.MongoClient("mongodb+srv://VIT_Admin:pizza@vitdiaries.tpuku.mongodb.net/vitd?retryWrites=true&w=majority")
+db = cluster["vitd"]
+collection = db["posts"]
+post = [{
+        'Author' : 'Anonymous',
+        'Text' : 'Demo Text'
+        },
+        {
+        'Author' : 'Sample',
+        'Text' : 'Demo Text 2'
+        },
+        {
+        'Author' : 'No',
+        'Text' : 'Demo Text 3'
+        }]
+collection.insert_many(post)

b5d66e76e9a70bf9acab4644a08fcef7b84b0184
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-25 12:45:20 +0530

    NewPost Functional now

--- fetch.py
+++ fetch.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_pymongo import pymongo
 app = Flask(__name__)
 CONNECTION_STRING = "mongodb+srv://VIT_Admin:pizza@vitdiaries.tpuku.mongodb.net/vitd?retryWrites=true&w=majority"
@@ -6,8 +6,8 @@ client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('vitd')
 user_collection = pymongo.collection.Collection(db, 'posts')
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def home():
     post_all = (list(user_collection.find({}, {'_id':0})))
     # print(post_all)
     return render_template("index.html", post_all = post_all)
@@ -24,8 +24,14 @@ def newss():
 def contactus():
     return render_template("contact.html")
 
-@app.route('/newpost')
+@app.route('/newpost', methods=['GET', 'POST'])
 def newpost():
+    author = request.form.get('post-author')
+    text = request.form.get('post-content')
+    print(author)
+    print(text)
+    if (author != None or text !=None):
+        user_collection.insert_one({'Author': author, 'Text':text})
     return render_template("newpost.html")
 
 if __name__ == "__main__":
